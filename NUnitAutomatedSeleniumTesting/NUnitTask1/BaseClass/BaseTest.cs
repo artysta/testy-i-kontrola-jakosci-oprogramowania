6886b7249d6f5db27c7a2fa4ba07bc771d9cca18
Adrian Kurek <adrian.kurek93@gmail.com>
2021-01-19 20:30:55 +0100

    Add task 1 result

---
+++ NUnitAutomatedSeleniumTesting/NUnitTask1/BaseClass/BaseTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace NUnitTask1.BaseClass
+{
+	public class BaseTest
+	{
+		public IWebDriver driver;
+
+		[OneTimeSetUp]
+		public void Open()
+		{
+			driver = new ChromeDriver();
+			driver.Manage().Window.Maximize();
+			driver.Url = "https://www.facebook.com/register";
+		}
+
+		[OneTimeTearDown]
+		public void Close()
+		{
+			driver.Quit();
+		}
+	}
+}
---
+++ NUnitAutomatedSeleniumTesting/NUnitTask1/TestClass.cs
@@ -0,0 +1,35 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using NUnit.Framework;
+using NUnitTask1.BaseClass;
+using OpenQA.Selenium;
+using System.Threading;
+
+namespace NUnitTask1
+{
+	[TestFixture]
+    public class TestClass : BaseTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            IWebElement emailTextField = driver.FindElement(By.XPath("//*[@id='u_0_s']"));
+            emailTextField.SendKeys("Selenium C#");
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            IWebElement emailTextField = driver.FindElement(By.XPath("//*[@id='u_0_s']"));
+            emailTextField.SendKeys("Selenium C#");
+        }
+
+        [Test]
+        public void TestMethod3()
+        {
+            IWebElement emailTextField = driver.FindElement(By.XPath("//*[@id='u_0_s']"));
+            emailTextField.SendKeys("Selenium C#");
+            Thread.Sleep(5000);
+        }
+    }
+}
