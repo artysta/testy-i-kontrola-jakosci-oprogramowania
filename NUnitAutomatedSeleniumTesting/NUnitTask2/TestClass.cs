c179f323d477bf66af036752bdebb5049c783d89
Adrian Kurek <adrian.kurek93@gmail.com>
2021-01-20 20:20:21 +0100

    Add task 2 result

---
+++ NUnitAutomatedSeleniumTesting/NUnitTask2/TestClass.cs
@@ -0,0 +1,42 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using NUnit.Framework;
+using NUnitTask1.BaseClass;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
+namespace NUnitTask2
+{
+    [TestFixture]
+    public class TestClass : BaseTest
+    {
+        [Test, Category("Smoke Testing")]
+        public void TestMethod1()
+        {
+            IWebElement emailTextField = driver.FindElement(By.XPath("//*[@id='u_0_s']"));
+            emailTextField.SendKeys("Selenium C#");
+
+            IWebElement monthDropdownList = driver.FindElement(By.XPath("//*[@id='month']"));
+            SelectElement element = new SelectElement(monthDropdownList);
+            element.SelectByIndex(2);
+            element.SelectByText("Mar");
+            element.SelectByValue("6");
+        }
+
+        [Test, Category("Regression Testing")]
+        public void TestMethod2()
+        {
+            IWebElement emailTextField = driver.FindElement(By.XPath("//*[@id='u_0_s']"));
+            emailTextField.SendKeys("Selenium C#");
+        }
+
+        [Test, Category("Smoke Testing")]
+        public void TestMethod3()
+        {
+            IWebElement emailTextField = driver.FindElement(By.XPath("//*[@id='u_0_s']"));
+            emailTextField.SendKeys("Selenium C#");
+            Thread.Sleep(5000);
+        }
+    }
+}
